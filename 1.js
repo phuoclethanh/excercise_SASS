ac7ea0e0dea6c1b45a6df961f92c3d13e98bc824
phuoclethanh <lethanhphuocdktd@gmail.com>
2022-05-08 10:01:49 +0700

    sass

---
+++ 1.js
@@ -0,0 +1,9 @@
+﻿$(function(){
+ 	$('.cacngonngu').slideUp();
+ 	$('.top-ngonngu b').click(function(event) {
+ 		console.log('1');
+ 		$(this).next().slideToggle(500);
+ 	});
+
+ 	new WOW().init();
+})  
